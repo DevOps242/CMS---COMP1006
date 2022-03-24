541b2ea7ae9a06a4fa7cc16dd178447be53a98b9
Akeem Palmer <akeemk.palmer@gmail.com>
2022-03-24 10:52:05 -0400

    Update: created Log class to generate logs

---
+++ utilities/Logs.php
@@ -0,0 +1,27 @@
+<?php 
+
+class Log {
+
+    function __construct() {
+
+    }
+
+    public function error($message) {
+        // takes in the error 
+
+        // Grabs the current date. 
+
+        // Grabs the users session if any, 
+
+        // Grabs the users time, 
+        
+        // Creats a .log file if not already created for the date. 
+
+
+        // Append the message if not already done.
+    }
+
+    public function log($message) {
+
+    }
+}
