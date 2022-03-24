e192d82243e67553e0baec7291a9d263be49f1fb
Akeem Palmer <42576837+DevOps242@users.noreply.github.com>
2022-03-24 14:40:18 -0400

    Delete shared.php

--- utilities/shared.php
+++
@@ -1,7 +0,0 @@
-<?php 
-define('DB_HOST', '172.31.22.43');
-define('DB_NAME', 'Akeem1158892');
-define('DB_USERNAME', 'Akeem1158892');
-define('DB_PASSWORD', 'mnT2hBL1ps');
-define('BASE_URL', 'http://localhost:8888/Assignments/CMS%20-%20Assignment%202/');
-
